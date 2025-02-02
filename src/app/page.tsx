3dcd0714980552c0e969229027db74dfa8d636d6
shreyasshirwadkar <shreyas.shirwadkar1@gmail.com>
2025-02-02 05:39:18 +0530

    Created Pricing section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,11 @@ import { Header } from "@/sections/Header";
 import { Hero } from "@/sections/Hero";
 import { LogoTicker } from "@/sections/LogoTicker";
 import { ProductShowcase } from "@/sections/ProductShowcase";
+import { Pricing } from "@/sections/Pricing";
 export default function Home() {
   return (
     <>
-      <Header /> <Hero /> <LogoTicker /> <ProductShowcase />
+      <Header /> <Hero /> <LogoTicker /> <ProductShowcase /> <Pricing/>
     </>
   );
 }
