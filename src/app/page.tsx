be50c109263d334fdc0aa5e96a5e9370ca4dc4e9
shreyasshirwadkar <shreyas.shirwadkar1@gmail.com>
2025-02-02 06:50:40 +0530

    Footer added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,12 @@ import { ProductShowcase } from "@/sections/ProductShowcase";
 import { Pricing } from "@/sections/Pricing";
 import { Testimonials } from "@/sections/Testimonials";
 import { CallToAction } from "@/sections/CallToAction";
+import { Footer } from "@/sections/Footer";
 export default function Home() {
   return (
     <>
       <Header /> <Hero /> <LogoTicker /> <ProductShowcase /> <Pricing />{" "}
-      <Testimonials /> <CallToAction />
+      <Testimonials /> <CallToAction /> <Footer />
     </>
   );
 }
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,40 @@
+import logo from "@/assets/logosaas.png";
+import Socialx from "@/assets/social-x.svg";
+import Socialinsta from "@/assets/social-insta.svg";
+import SocialLinkedin from "@/assets/social-linkedin.svg";
+import SocialPin from "@/assets/social-pin.svg";
+import SocialYt from "@/assets/social-youtube.svg";
+import Image from "next/image";
 export const Footer = () => {
-  return null;
+  return (
+    <footer className="bg-black text-[#BCBCBC] text-sm py-10 text-center">
+      <div className="px-1">
+        <div className="inline-flex relative before:top-2 before:bottom-0 before:blur before:content-[''] before:h-full before:w-full before:bg-[linear-gradient(to_right,#F87BFF,#FB92CF,#FFDD9B,#C2F0B1,#2FD8FE)] before:absolute">
+          <Image
+            src={logo}
+            alt="logo"
+            height={40}
+            width={40}
+            className="relative"
+          />
+        </div>
+        <nav className="flex flex-col md:flex-row justify-center gap-6 mt-6">
+          <a href="#">About</a>
+          <a href="#">Features</a>
+          <a href="#">Customers</a>
+          <a href="#">Pricing</a>
+          <a href="#">Help</a>
+          <a href="#">Careers</a>
+        </nav>
+        <div className="flex justify-center gap-6 mt-6">
+          <Socialx />
+          <Socialinsta />
+          <SocialYt />
+          <SocialPin />
+          <SocialLinkedin />
+        </div>
+        <p className="mt-6">&copy; 2025 Leo,Inc. All rights reserved.</p>
+      </div>
+    </footer>
+  );
 };
